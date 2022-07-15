06f75964b0eeddaa8811f16e11d31549d3ce6982
Augusto César <negocios.augusto@gmail.com>
2022-07-15 18:21:10 -0300

    criado repositório

---
+++ src/test/java/com/augusto/os/OsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augusto.os;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
